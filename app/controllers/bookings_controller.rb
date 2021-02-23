2de4a5ae035f28989411b5a345f7c6157c956e75
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-23 18:58:46 +0100

    link to my_bookings fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,20 @@
 class BookingsController < ApplicationController
-
-    def create
-    @boat = Boat.find(params[:boat_id])
-    @booking = Booking.new(booking_params)
-    @booking.boat = @boat
-    @booking.user = current_user
-    if @booking.save
-      flash[:notice] = "Booking for #{@boat.name} complete!"
-      redirect_to boat_path(@boat)
-    else
-      render "boats/show"
-    end
+  def my_bookings
+    @bookings = Booking.where(user: current_user)
   end
+
+  def create
+  @boat = Boat.find(params[:boat_id])
+  @booking = Booking.new(booking_params)
+  @booking.boat = @boat
+  @booking.user = current_user
+  if @booking.save
+    flash[:notice] = "Booking for #{@boat.name} complete!"
+    redirect_to boat_path(@boat)
+  else
+    render "boats/show"
+  end
+end
 
   private
 
@@ -19,8 +22,5 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:starting_date, :ending_date)
   end
 
-  def my_bookings
-    @bookings = Booking.where(user: current_user)
-  end
 end
 
