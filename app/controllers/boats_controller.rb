6cc295721b4d5b2cf9b722038a5cae5c608f5a3d
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-23 12:45:09 +0100

    added edit and update actions + edit form in edit view

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,25 @@
 class BoatsController < ApplicationController
+
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(boat_params)
+
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :edit
+    end
+  end
+
+private
+
+def boat_params
+  params.require(:boat).permit(:name, :decription, :daily_rate)
+end
+
+
 end
