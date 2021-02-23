bcdc2f8c9a982ae93578e124de64d4ee1c2d8ede
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-23 18:23:43 +0100

    no authentication for show page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,5 @@
 class BoatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def new
     @boat = Boat.new
