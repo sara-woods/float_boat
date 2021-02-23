4f5e0266c1203546588843e328e85b89df2f6723
Gabriel Hardy-Francon <gabrielhardyfrancon@gmail.com>
2021-02-23 13:11:28 +0100

    new and create methods added

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,20 @@
 class BoatsController < ApplicationController
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to boats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :address, :description, :daily_rate)
+  end
 end
