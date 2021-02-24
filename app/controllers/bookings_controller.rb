e0d73ba2ce5b27d59b33b9ef98508f5756a06d78
Erickson P <joelerickson@hotmail.co.uk>
2021-02-24 13:02:57 +0000

    added my_boats page, edited delete method in boats controller, added list, edit button and delete button with alert

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,18 +3,22 @@ class BookingsController < ApplicationController
     @bookings = Booking.where(user: current_user)
   end
 
-  def create
-  @boat = Boat.find(params[:boat_id])
-  @booking = Booking.new(booking_params)
-  @booking.boat = @boat
-  @booking.user = current_user
-  if @booking.save
-    flash[:notice] = "Booking for #{@boat.name} complete!"
-    redirect_to my_bookings_path 
-  else
-    render "boats/show"
+  def my_boats
+    @boats = current_user.boats
   end
-end
+
+  def create
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new(booking_params)
+    @booking.boat = @boat
+    @booking.user = current_user
+    if @booking.save
+      flash[:notice] = "Booking for #{@boat.name} complete!"
+      redirect_to my_bookings_path
+    else
+      render "boats/show"
+    end
+  end
 
   private
 
