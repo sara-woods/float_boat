d16ab006598a45b01f840c10711e94061e462a69
Luke Billings <lukecbillings@gmail.com>
2021-02-25 11:55:07 +0000

    address to coordinates working

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -7,4 +7,7 @@ class Boat < ApplicationRecord
   validates :description, presence: true
   validates :daily_rate, presence: true
   validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
